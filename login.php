5255611a739fd8ac8edc390b64606dca10834e96
Nishit Khalpada <58837605+nishit1617@users.noreply.github.com>
2020-10-23 13:15:03 +0530

    Update login.php
    Fix Icon issue and add new login page

--- login.php
+++ login.php
@@ -15,31 +15,28 @@
                 <div class="text-center">
                   <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                 </div>
-                <form class="user">
+                <form>
                   <div class="form-group">
-                    <input type="email" class="form-control form-control-user" id="exampleInputEmail"
-                      aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                   </div>
                   <div class="form-group">
-                    <input type="password" class="form-control form-control-user" id="exampleInputPassword"
-                      placeholder="Password">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                   </div>
-                  <div class="form-group">
-                    <div class="custom-control custom-checkbox small">
-                      <input type="checkbox" class="custom-control-input" id="customCheck">
-                      <label class="custom-control-label" for="customCheck">Remember Me</label>
-                    </div>
+                  <div class="form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
                   </div>
-                  <a href="index.html" class="btn btn-primary btn-user btn-block">
-                    Login
-                  </a>
-                  <hr>
-                  <a href="index.html" class="btn btn-google btn-user btn-block">
-                    <i class="fab fa-google fa-fw"></i> Login with Google
-                  </a>
-                  <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                    <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                  </a>
+                  <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+                  <button class="button">
+                    <a href="https://www.google.com/" class="fa fa-google" type="button">Login with google</a>
+                  </button>
+                 <button class="button">
+                    <a href="https://www.facebook.com/" class="fa fa-facebook" type="button">Login with facebook</a>
+                 </button>
                 </form>
                 <hr>
                 <div class="text-center">
